153355c51d2095aaf4ab571b8572a0b68dcbabba
Shengzhe Yao <shengzhe@shengzhe-macbookpro.roam.corp.google.com>
2018-12-09 19:45:16 -0800

    Set up Bazel.

---
+++ src/com/yaoshengzhe/paxos/ClusterMain.java
@@ -0,0 +1,7 @@
+package com.yaoshengzhe.paxos;
+
+public class ClusterMain {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/com/yaoshengzhe/paxos/Node.java
@@ -0,0 +1,4 @@
+package com.yaoshengzhe.paxos;
+
+public class Node {
+}
